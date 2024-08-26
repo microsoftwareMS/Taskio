b8eeb5f3b8ce5220515e1772da24410c68f35b1c
microsoftwareMS <117680033+microsoftwareMS@users.noreply.github.com>
2024-08-26 12:14:09 -0500

    home.js

--- js/home.js
+++ js/home.js
@@ -10,18 +10,4 @@ document.addEventListener('DOMContentLoaded', () => {
     if (savedTheme) {
         body.classList.toggle('dark-mode', savedTheme === 'dark');
     }
-
-    // Cambiar el tema cuando se haga clic en el botón
-    themeToggleButton.addEventListener('click', () => {
-        const isDarkMode = body.classList.toggle('dark-mode');
-        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-        const inChargerElements = document.querySelectorAll('.in-charger');
-        inChargerElements.forEach(inChargerDiv => {
-            const handInHomeworkDiv = inChargerDiv.querySelector('.hand-in-homework');
-            const bgColor = handInHomeworkDiv && handInHomeworkDiv.style.display === 'block'
-                ? (isDarkMode ? '#111111' : '#e0e0e0')
-                : '';
-            inChargerDiv.style.backgroundColor = bgColor;
-        });
-    });
 });
